486e07fc8843883fb274e8111d9679099dc27596
vgolubnichiy <vitaliy.golubnichiy@softgamings.com>
2021-12-22 18:59:09 +0300

    Lesson 3 commit

---
+++ NewFolder1/Tests.cs
@@ -0,0 +1,25 @@
+﻿using adressbook_web_tests;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1.NewFolder1
+{
+    public class Tests
+    {
+
+        [Test]
+        public void Method()
+        {
+            UntitledTestCase d = new UntitledTestCase();
+            d.SetupTest();
+            d.TheUntitledTestCaseTest();
+            d.TeardownTest();
+        }
+    }
+
+
+}
